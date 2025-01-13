65d62c15df18244584f0facadcdcc8734932ce62
andria.gv <gvaramiaandria1@gmail.com>
2025-01-13 14:18:10 +0400

    add: dark mode support

--- FinalProject/FinalProject/Managers/DarkModeManager.swift
+++ FinalProject/FinalProject/Managers/DarkModeManager.swift
@@ -5,4 +5,39 @@
 //  Created by Apple on 12.01.25.
 //
 
-import Foundation
+import UIKit
+
+protocol ThemeManaging {
+    var currentTheme: AppTheme { get }
+    func saveTheme(_ theme: AppTheme)
+    func applyTheme(to window: UIWindow?)
+}
+
+final class DarkModeManager: ThemeManaging {
+    private let themeKey = "selectedTheme"
+    static let shared = DarkModeManager()
+
+    var currentTheme: AppTheme {
+        let savedTheme = UserDefaults.standard.string(forKey: themeKey) ?? AppTheme.light.rawValue
+        return AppTheme(rawValue: savedTheme) ?? .light
+    }
+
+    func saveTheme(_ theme: AppTheme) {
+        UserDefaults.standard.set(theme.rawValue, forKey: themeKey)
+    }
+
+    func applyTheme(to window: UIWindow?) {
+        guard let window = window else { return }
+        switch currentTheme {
+        case .light:
+            window.overrideUserInterfaceStyle = .light
+        case .dark:
+            window.overrideUserInterfaceStyle = .dark
+        }
+    }
+}
+
+enum AppTheme: String, CaseIterable {
+    case light = "Light"
+    case dark = "Dark"
+}
--- FinalProject/FinalProject/Root/Profile/ProfileViewModel.swift
+++ FinalProject/FinalProject/Root/Profile/ProfileViewModel.swift
@@ -5,4 +5,22 @@
 //  Created by Apple on 12.01.25.
 //
 
-import Foundation
+import SwiftUI
+
+final class ProfileViewModel: ObservableObject {
+    @Published var selectedTheme: AppTheme
+    private let themeManager: ThemeManaging
+    
+    
+    init(themeManager: ThemeManaging = DarkModeManager.shared) {
+        self.themeManager = themeManager
+        self.selectedTheme = themeManager.currentTheme
+    }
+    
+    func setTheme(_ theme: AppTheme, for window: UIWindow?) {
+        self.selectedTheme = theme
+        themeManager.saveTheme(theme)
+        themeManager.applyTheme(to: window)
+    }
+}
+
