71834a89a1fab456a95993c88f5adaa1dda7d0a0
andria.gv <gvaramiaandria1@gmail.com>
2025-01-24 16:46:58 +0400

    convert like page code from uikit to swiftUI

---
+++ FinalProject/FinalProject/Root/Like/LikesPageView.swift
@@ -0,0 +1,46 @@
+//
+//  LikesPageView.swift
+//  FinalProject
+//
+//  Created by Apple on 24.01.25.
+//
+
+import SwiftUI
+
+
+struct LikesPageView: View {
+    @StateObject private var viewModel = LikesViewModel()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                if viewModel.isLoading {
+                    ProgressView()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                } else {
+                    LazyVStack(spacing: 16) {
+                        ForEach(viewModel.likedEvents) { event in
+                            LikedEventCard(event: event, onUnlike: {
+                                Task {
+                                    try? await viewModel.unlikeEvent(event)
+                                }
+                            })
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .background(Color(.systemGroupedBackground))
+            .navigationTitle("ფავორიტები")
+            .overlay(Group {
+                if viewModel.likedEvents.isEmpty && !viewModel.isLoading {
+                    EmptyStateView()
+                }
+            })
+        }
+    }
+}
+
+#Preview {
+    LikesPageView()
+}
