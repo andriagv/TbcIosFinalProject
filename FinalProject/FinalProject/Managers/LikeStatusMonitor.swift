ea4b5353ed93399702766c98218469397c33624c
andria.gv <gvaramiaandria1@gmail.com>
2025-01-24 16:50:16 +0400

    add like status manager

---
+++ FinalProject/FinalProject/Managers/LikeStatusMonitor.swift
@@ -0,0 +1,18 @@
+//
+//  LikeStatusMonitor.swift
+//  FinalProject
+//
+//  Created by Apple on 24.01.25.
+//
+
+import Foundation
+
+
+final class LikeStatusMonitor: ObservableObject {
+    @Published var lastUpdated = Date()
+    static let shared = LikeStatusMonitor()
+    
+    func statusChanged() {
+        lastUpdated = Date()
+    }
+}
